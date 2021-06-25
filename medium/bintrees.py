136ce53f72f8c1601cafc73804333156e1185bfc
a-gon <annab.goncharova@gmail.com>
2021-06-25 09:56:16 -0700

    Updates possible cases for lca problem

--- medium/bintrees.py
+++ medium/bintrees.py
@@ -1,4 +1,12 @@
 def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    ''' 
+    cases:
+    1. not L and not R >> return none
+    2. root == to p or q >> return root
+    3. L and R >> return root
+    4. L or R >> return L or R wichever is not none
+    
+    '''
     if not root:
         return None
 
