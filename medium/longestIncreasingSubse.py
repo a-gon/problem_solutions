3d8ac2f964ef3f08410391d9d3037866880bcafe
a-gon <annab.goncharova@gmail.com>
2021-07-14 15:27:56 -0700

    Added another solution

--- medium/longestIncreasingSubse.py
+++ medium/longestIncreasingSubse.py
@@ -16,3 +16,30 @@ assert(longestIncreasingSubseq([5, 7, -24, 12, 10, 2, 3, 12, 5, 6, 35]) == [-24,
 assert(longestIncreasingSubseq([3, 2, 1]) == [3]), 'Incorrect result in test case 2'
 
         
+def longestIncreasingSubsequence(array):
+    ''' Another solution, more efficient in terms of space - sttores onle the indices needed for recreating the resulting sequence'''
+    if not array:
+	    return array
+    lengths = [1] * len(array)
+    sequences = [None] * len(array)
+    maxLenIdx = 0
+    for i in range(len(array)):
+	    for j in range(i):
+		    if array[j] < array[i] and lengths[j] + 1 >= lengths[i]:
+			    lengths[i] = lengths[j] + 1
+			    sequences[i] = j
+	    if lengths[i] >= lengths[maxLenIdx]:
+		    maxLenIdx = i
+
+    return buildSeqs(array, sequences, maxLenIdx)
+
+def buildSeqs(array, sequences, startIdx):
+	result = []
+	while startIdx is not None:
+		result.append(array[startIdx])
+		startIdx = sequences[startIdx]
+	return list(reversed(result))
+
+
+assert(longestIncreasingSubsequence([5, 7, -24, 12, 10, 2, 3, 12, 5, 6, 35]) == [-24, 2, 3, 5, 6, 35]), 'Incorrect result in test case 1'
+assert(longestIncreasingSubsequence([3, 2, 1]) == [3] or [1] or [2]), 'Incorrect result in test case 2'
